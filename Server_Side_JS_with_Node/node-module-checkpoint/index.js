0884f3f79921950862cad23e94f1076c2b9c6c27
Nicole Speirs <nickispeirs@gmail.com>
2019-07-11 16:47:57 -0500

    add uuid to index.js

--- Server_Side_JS_with_Node/node-module-checkpoint/index.js
+++ Server_Side_JS_with_Node/node-module-checkpoint/index.js
@@ -1,9 +1,7 @@
 // you're requiring or importing the my-library module, which you previously exported here
 const myLibrary = require('./my-library');
-// execute my-library here
-myLibrary.myFirstMethod();
-
 const uuid = require('uuid');
 const newUuid = uuid();
-
+// execute my-library here
+myLibrary.myFirstMethod();
 console.log(`your new Universally unique identifier is ${newUuid}`);
