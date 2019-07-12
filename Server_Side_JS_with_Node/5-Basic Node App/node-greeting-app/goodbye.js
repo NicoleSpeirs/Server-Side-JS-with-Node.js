d7978aa6e4b534cacb5e9f87b3fdcd477d9de182
Nicole Speirs <nickispeirs@gmail.com>
2019-07-12 15:28:11 -0500

    add basic node app: complete

---
+++ Server_Side_JS_with_Node/5-Basic Node App/node-greeting-app/goodbye.js
@@ -0,0 +1,15 @@
+exports.goodbyeEnglish = function() {
+  console.log("Goodbye");
+}
+
+exports.goodbyeJapanese = function() {
+  console.log("Sayōnara");
+}
+
+exports.goodbyeSpanish = function() {
+  console.log("Adiós");
+}
+
+exports.goodbyeIcelandic = function() {
+  console.log("Vertu blessaður");
+}
---
+++ Server_Side_JS_with_Node/5-Basic Node App/node-greeting-app/main.js
@@ -0,0 +1,31 @@
+const greetings = require("./greeting.js");
+const goodbyes = require("./goodbye.js");
+
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Halló"
+greetings.greetingIcelandic();
+
+//"Goodbye"
+goodbyes.goodbyeEnglish();
+
+//"Sayōnara"
+goodbyes.goodbyeJapanese();
+
+//"Adiós"
+goodbyes.goodbyeSpanish();
+
+//"Vertu blessaður"
+goodbyes.goodbyeIcelandic();
+
+
+
+
