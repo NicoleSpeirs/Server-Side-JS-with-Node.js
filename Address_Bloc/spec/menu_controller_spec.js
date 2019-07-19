696d4aa0bbf0408e2f56f22bc2397a0627593ea2
Nicole Speirs <nickispeirs@gmail.com>
2019-07-19 13:36:21 -0500

    add specs to Menu Controller

---
+++ Address_Bloc/spec/menu_controller_spec.js
@@ -0,0 +1,25 @@
+const MenuController = require("../controllers/MenuController");
+// #1
+describe("MenuController", () => {
+  describe("#getContactCount()", () => {
+    beforeEach(() => {
+      this.menu = new MenuController();
+    });
+    describe("#remindMe()", () => {
+
+    });
+
+// #2
+    it("should return 0 when no contacts are in the book", () => {
+      expect(this.menu.getContactCount()).toBe(0);
+    });
+    it("should return 1 when there is exactly one contact in the book", () => {
+      this.menu.contacts.push("Bob");
+      expect(this.menu.getContactCount()).toBe(1)
+    });
+    it("should return 'Learning is a life-long pursuit'", () => {
+      expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit")
+    });
+  });
+
+});
