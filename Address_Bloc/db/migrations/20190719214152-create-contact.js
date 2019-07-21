2853c640579812fa95ca19c6c53c359059f8ef1d
Nicole Speirs <nickispeirs@gmail.com>
2019-07-21 15:04:03 -0500

    creating contacts and connecting to a database: started

---
+++ Address_Bloc/db/migrations/20190719214152-create-contact.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Contacts', {
+      //first arg is name of table, second is object of key-value pairs
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Contacts');
+  }
+};
