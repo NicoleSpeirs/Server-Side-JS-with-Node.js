04da060659264e098f94cf76677df5f3f151c18b
Nicole Speirs <nickispeirs@gmail.com>
2019-07-21 21:56:17 -0500

    add email column to the contacts tablet: assignment complete

--- Address_Bloc/controllers/ContactController.js
+++ Address_Bloc/controllers/ContactController.js
@@ -2,15 +2,14 @@ const inquirer = require("inquirer");
 const Contact = require("../db/models").Contact;
 
 module.exports = class ContactController {
-
-  constructor(){
+  constructor() {
     this.contacts = [];
     this.addContactQuestions = [
       {
         type: "input",
         name: "name",
         message: "Contact's name - ",
-        validate(val){
+        validate(val) {
           return val !== "";
         }
       },
@@ -18,15 +17,22 @@ module.exports = class ContactController {
         type: "input",
         name: "phone",
         message: "Contact's phone number - ",
-        validate(val){
+        validate(val) {
+          return val !== "";
+        }
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Contact's email - ",
+        validate(val) {
           return val !== "";
         }
       }
     ];
-
   }
 
-  addContact(name, phone){
-    return Contact.create({name, phone})
+  addContact(name, phone, email) {
+    return Contact.create({ name, phone, email });
   }
 };
