160415a1de169ad832e3aab191510f8cf9791373
Nicole Speirs <nickispeirs@gmail.com>
2019-07-24 21:31:58 -0500

    listing contacts and searching checkpoint: complete

--- Address_Bloc/controllers/MenuController.js
+++ Address_Bloc/controllers/MenuController.js
@@ -8,7 +8,13 @@ module.exports = class MenuController {
         type: "list",
         name: "mainMenuChoice",
         message: "Please choose from an option below: ",
-        choices: ["Add new contact", "Get current Date and time", "Exit"]
+        choices: [
+          "Add new contact",
+          "View all contacts",
+          "Search for a contact",
+          "Get current Date and time",
+          "Exit"
+        ]
       }
     ];
     this.book = new ContactController();
@@ -23,6 +29,12 @@ module.exports = class MenuController {
           case "Add new contact":
             this.addContact();
             break;
+          case "View all contacts":
+            this.getContacts();
+            break;
+          case "Search for a contact":
+            this.search();
+            break;
           case "Get current Date and time":
             this.getDate();
             break;
@@ -32,9 +44,9 @@ module.exports = class MenuController {
             console.log("Invalid input");
         }
       })
-      .then(() => {
-        this.main();
-      })
+      // .then(() => {
+      //   this.main();
+      // })
       .catch(err => {
         console.log(err);
       });
@@ -58,7 +70,7 @@ module.exports = class MenuController {
           this.main();
         });
     });
-    this.main();
+    // this.main();
   }
 
   exit() {
@@ -78,5 +90,91 @@ module.exports = class MenuController {
     var d = Date(Date.now());
     var a = d.toString();
     console.log("The current date is: " + a);
+  }
+
+  search() {
+    inquirer
+      .prompt(this.book.searchQuestions)
+      .then(target => {
+        this.book.search(target.name).then(contact => {
+          if (contact === null) {
+            this.clear();
+            console.log("contact not found");
+            this.search();
+          } else {
+            this.showContact(contact);
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.main();
+      });
+  }
+
+  showContact(contact) {
+    this._printContact(contact);
+    inquirer
+      .prompt(this.book.showContactQuestions)
+      .then(answer => {
+        switch (answer.selected) {
+          case "Delete contact":
+            this.delete(contact);
+            break;
+          case "Main menu":
+            this.main();
+            break;
+          default:
+            console.log("Something went wrong.");
+            this.showContact(contact);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.showContact(contact);
+      });
+  }
+
+  _printContact(contact) {
+    console.log(`
+    name: ${contact.name}
+    phone number: ${contact.phone}
+    email: ${contact.email}
+    ---------------`);
+  }
+
+  getContacts() {
+    this.clear();
+
+    this.book
+      .getContacts()
+      .then(contacts => {
+        for (let contact of contacts) {
+          this._printContact(contact);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.main();
+      });
+  }
+
+  delete(contact) {
+    inquirer
+      .prompt(this.book.deleteConfirmQuestions)
+      .then(answer => {
+        if (answer.confirmation) {
+          this.book.delete(contact.id);
+          console.log("contact deleted!");
+          this.main();
+        } else {
+          console.log("contact not deleted");
+          this.showContact(contact);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.main();
+      });
   }
 };
